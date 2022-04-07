1668b1de09ec057a264b9dee1e2995ecbb47a6eb
Brian Li <brnli7@gmail.com>
2022-04-07 14:55:12 -0700

    Add decode-register task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { task } from "hardhat/config";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
+import { NomRegistrarController__factory } from "./typechain/factories/NomRegistrarController__factory";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -12,6 +13,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
+task("decode-register", "Decode register")
+  .addParam("data", "data to decode")
+  .setAction(async ({ data }) => {
+    const f = NomRegistrarController__factory.createInterface();
+    const decoded = f.decodeFunctionData("registerWithConfig", data);
+    console.log(decoded);
+  });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
